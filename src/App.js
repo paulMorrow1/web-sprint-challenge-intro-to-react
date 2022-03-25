c779f355e47f483878422aec8ba8f1315e8438a5
Paul Morrow <pmorow93@gmail.com>
2022-03-25 10:56:16 -0500

    useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Character from "./components/Character";
+import axios from "axios";
 
 const App = () => {
+  const [data, setData] = useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people")
+      .then((res) => {
+        console.log(res.data.results);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,8 @@
 // Write your Character component here
+import React from "react";
+
+const Character = (props) => {
+  return <div></div>;
+};
+
+export default Character;
